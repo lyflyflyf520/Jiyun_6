786ad7d31d8fc99b9fbca95973e88ccaed36adfc
chentaishan <chentaishan-19@163.com>
2019-01-04 15:39:07 +0800

    update name

--- jiyun_6/src/main/java/com/jiyun/com/jiyun_z5/utils/Urls.java
+++ jiyun_6/src/main/java/com/jiyun/com/jiyun_z5/utils/Urls.java
@@ -6,17 +6,17 @@ import java.util.List;
 
 public class Urls {
 
-    public static final String  PAKEAGE_NAME="com.jiyun.com.jiyun_z5.";
-     public static final List<HashMap<String,String>> classList = new ArrayList<>();
+    public static final String PAKEAGE_NAME = "com.jiyun.com.jiyun_z5.";
+    public static final List<HashMap<String, String>> classList = new ArrayList<>();
 
     static {
-        HashMap<String,String>  className = new HashMap<>();
+        HashMap<String, String> className = new HashMap<>();
 
-        className.put("XRecyclerViewActivity","test xRecyclerview");
+        className.put("XRecyclerViewActivity", "1.okhttp $ xRecyclerView GET POST 请求头 拦截器");
         classList.add(className);
 
         className = new HashMap<>();
-        className.put("TestRetrofitActivity","retrofit的get方式获取demo");
+        className.put("TestRetrofitActivity", "retrofit的get方式获取demo");
         classList.add(className);
 
     }
