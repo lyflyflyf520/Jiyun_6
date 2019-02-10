030c0247d71485935c14c711d983732f97d1e52d
chentaishan <chentaishan-19@163.com>
2019-02-10 11:24:06 +0800

    rxjava flowable 背压

--- day7/src/main/java/com/example/day7/MainActivity.java
+++ day7/src/main/java/com/example/day7/MainActivity.java
@@ -7,8 +7,15 @@ import android.util.Log;
 
 import com.rx2androidnetworking.Rx2AndroidNetworking;
 
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
 import java.io.IOException;
 
+import io.reactivex.BackpressureStrategy;
+import io.reactivex.Flowable;
+import io.reactivex.FlowableEmitter;
+import io.reactivex.FlowableOnSubscribe;
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
@@ -132,5 +139,44 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         return response;
+    }
+
+    //Flowable 发射数据完成以后再接收  参考：https://www.heqiangfly.com/2017/10/14/open-source-rxjava-guide-flowable/
+    private void testFlowable(){
+        Flowable.create(new FlowableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(FlowableEmitter<Integer> e) throws Exception {
+                System.out.println("发射----> 1");
+                e.onNext(1);
+                System.out.println("发射----> 2");
+                e.onNext(2);
+                System.out.println("发射----> 3");
+                e.onNext(3);
+                System.out.println("发射----> 完成");
+                e.onComplete();
+            }
+        }, BackpressureStrategy.BUFFER) //create方法中多了一个BackpressureStrategy类型的参数
+                .subscribeOn(Schedulers.newThread())//为上下游分别指定各自的线程
+                .observeOn(Schedulers.newThread())
+                .subscribe(new Subscriber<Integer>() {
+                    @Override
+                    public void onSubscribe(Subscription s) {   //onSubscribe回调的参数不是Disposable而是Subscription
+                        s.request(Long.MAX_VALUE);            //注意此处，暂时先这么设置
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        System.out.println("接收----> " + integer);
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        System.out.println("接收----> 完成");
+                    }
+                });
     }
 }
