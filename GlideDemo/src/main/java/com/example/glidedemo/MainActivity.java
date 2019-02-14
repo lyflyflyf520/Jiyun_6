31c8317ee550e556afa71ed573e17900fc3fa250
chents <chentaishan-19@163.com>
2019-02-14 22:11:46 +0800

    glide demo 圆形 圆角

--- GlideDemo/src/main/java/com/example/glidedemo/MainActivity.java
+++ GlideDemo/src/main/java/com/example/glidedemo/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.glidedemo;
 
+import android.content.Context;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -11,16 +14,23 @@ import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.load.engine.GlideException;
 import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.RequestOptions;
 import com.bumptech.glide.request.target.Target;
+
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     /**
      * Hello World!
      */
     private ImageView mImg;
+    private ImageView mImg1;
+    private ImageView mImg2;
 
-    String imgUrl="http://www.wanandroid.com/blogimgs/50c115c2-cf6c-4802-aa7b-a4334de444cd.png";
+    String imgUrl="https://www.qubaobei.com/ios/cf/uploadfile/132/9/8289.jpg";
+//    String imgUrl="https://i1.mifile.cn/f/i/2018/mi8/summary/index2.jpg";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,29 +38,57 @@ public class MainActivity extends AppCompatActivity {
         initView();
 
 
+        loadCircleImage(this,imgUrl,mImg1);
+        loadCustRoundCircleImage(this,imgUrl,mImg2, RoundedCornersTransformation.CornerType.ALL);
+
+        RequestOptions mRequestOptions = RequestOptions.circleCropTransform()
+                .diskCacheStrategy(DiskCacheStrategy.NONE)//不做磁盘缓存
+                .skipMemoryCache(true);//不做内存缓存
+
+
         GlideApp.with(this)
                 .load(imgUrl)
                 .centerCrop()
+                .apply(mRequestOptions)
                 .placeholder(R.drawable.ic_launcher_background)//加载中显示的图片
                 .error(R.drawable.ic_launcher_foreground)// 错误后显示的图片
                 .diskCacheStrategy(DiskCacheStrategy.ALL)// 执行的缓存策略
                 .onlyRetrieveFromCache(true)   //使用缓存
-                .listener(new RequestListener<Drawable>() {// 图片加载 监听
-                    @Override
-                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
-                        return false;
-                    }
-
-                    @Override
-                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
-                        return false;
-                    }
-                })
                 .into(mImg);
 
     }
 
     private void initView() {
         mImg = (ImageView) findViewById(R.id.img);
+        mImg1 = (ImageView) findViewById(R.id.img1);
+        mImg2 = (ImageView) findViewById(R.id.img2);
     }
+    /*
+     *加载圆角图片-指定任意部分圆角（图片上、下、左、右四个角度任意定义）
+     */
+    public static void loadCustRoundCircleImage(Context context, String url, ImageView imageView, RoundedCornersTransformation.CornerType type) {
+        RequestOptions options = new RequestOptions()
+                .centerCrop()
+                .placeholder(android.R.color.white)
+                .error(android.R.color.black)
+                //.priority(Priority.HIGH)
+                .bitmapTransform(new RoundedCornersTransformation(45, 0, type))
+                .diskCacheStrategy(DiskCacheStrategy.ALL);
+
+        Glide.with(context).load(url).apply(options).into(imageView);
+    }
+    /*
+     *加载圆形图片
+     */
+    public static void loadCircleImage(Context context, String url, ImageView imageView) {
+        RequestOptions options = new RequestOptions()
+                .centerCrop()
+                .circleCrop()//设置圆形
+                .placeholder(android.R.color.white)
+                .error(android.R.color.black)
+                //.priority(Priority.HIGH)
+                .diskCacheStrategy(DiskCacheStrategy.ALL);
+        Glide.with(context).load(url).apply(options).into(imageView);
+    }
+
 }
