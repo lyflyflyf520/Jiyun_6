cf8bc84dda88730334c75a3c2a823e436aef4003
chentaishan <chentaishan-19@163.com>
2019-02-19 16:50:42 +0800

    glide deme  修改文件

--- GlideDemo/src/main/java/com/example/glidedemo/MainActivity.java
+++ GlideDemo/src/main/java/com/example/glidedemo/MainActivity.java
@@ -10,7 +10,9 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.RequestOptions;
 import com.bumptech.glide.request.target.Target;
 
 public class MainActivity extends AppCompatActivity {
